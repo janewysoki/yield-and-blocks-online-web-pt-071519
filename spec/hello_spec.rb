4bca1105b8a3825a96f40cbbddd6bd6de0b4082b
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-27 22:43:15 +0000

    Automatically backed up by Learn

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -4,7 +4,7 @@ describe "#hello_t" do
   let (:names) { ["Tim", "Tom", "Jim"] }
 
   it "calls the block once for each element in the passed-in array" do
-    expect{ hello_t(names){ |name| puts name } }
+    expect{ hello_t(names){ puts name } }
       .to output("Tim\nTom\nJim\n")
       .to_stdout
   end
